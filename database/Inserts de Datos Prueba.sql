e362ddd57e96bc481839f433d28d23818a44f239
Diego González Roca <Diegogr_97@hotmail.com>
2017-11-01 14:06:03 -0600

    Reorganización de el repositorio

---
+++ database/Inserts de Datos Prueba.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS empleado_laborando;
+
+INSERT INTO roles_de_sistema (id_de_rol, nombre_rol)
+        VALUES (56765,
+                'loco');
+				
+select * from roles_de_sistema;
