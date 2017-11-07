a6fabd1b34cb6bb5d875374a0a92ff50f67b9908
Diego González Roca <Diegogr_97@hotmail.com>
2017-11-07 11:26:10 -0600

    Cambio de título en Login

--- sistemaunis/index.php
+++ sistemaunis/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>Login</title>
+    <title>SISTEMAUNIS - Login</title>
     <link rel="stylesheet" href="css/app.css">
     
   </head>
